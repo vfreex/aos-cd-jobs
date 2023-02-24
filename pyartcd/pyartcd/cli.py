247db6e07c83851bfdf4d20c4a517869033e5571
Yuxiang Zhu <yuxzhu@redhat.com>
2023-02-24 16:25:46 +0800

    New signing job
    This will add a new job called sign-artifacts2 (we can continue to use the existing sign-artifacts job in case something goes wrong).
    
    This job never pulls artifacts from internet (release payloads from quay.io or client binaries on mirror.openshift.com) as the input for signing, but accepts artifacts through job parameters.
    
    It is completely implemented in Python and comes with a Jenkinsfile
    wrapper.
    
    promote-assembly job is also updated to pass those artifacts through parameters.
    
    Only supports product `openshift` at this moment.

--- pyartcd/pyartcd/cli.py
+++ pyartcd/pyartcd/cli.py
@@ -2,6 +2,7 @@ import asyncio
 from functools import update_wrapper
 import logging
 from pathlib import Path
+import types
 from typing import Optional
 
 import click
@@ -15,8 +16,6 @@ def click_coroutine(f):
     """ A wrapper to allow to use asyncio with click.
     https://github.com/pallets/click/issues/85
     """
-    f = asyncio.coroutine(f)
-
     def wrapper(*args, **kwargs):
         loop = asyncio.get_event_loop()
         return loop.run_until_complete(f(*args, **kwargs))
